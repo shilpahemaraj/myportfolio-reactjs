300b1f1b373bcf806959fd04368042f9827d5a9b
Shilpa Hemaraj <shilpahemaraj@Shilpas-MacBook-Pro.local>
2020-09-20 23:08:31 -0700

    Updates

---
+++ precache-manifest.cfc3e5dc7a9bdfa106b97ac32daa434f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "225ddb0a135c279fd04c59f066d2d69a",
+    "url": "/myportfolio-reactjs/index.html"
+  },
+  {
+    "revision": "64f83aa863d3830e7384",
+    "url": "/myportfolio-reactjs/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "ec12bf07d80e480b6d41",
+    "url": "/myportfolio-reactjs/static/css/main.03695f87.chunk.css"
+  },
+  {
+    "revision": "64f83aa863d3830e7384",
+    "url": "/myportfolio-reactjs/static/js/2.17f4f42f.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/myportfolio-reactjs/static/js/2.17f4f42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec12bf07d80e480b6d41",
+    "url": "/myportfolio-reactjs/static/js/main.028f1c2d.chunk.js"
+  },
+  {
+    "revision": "4f79903d1e63da7150b3",
+    "url": "/myportfolio-reactjs/static/js/runtime-main.e10195b7.js"
+  },
+  {
+    "revision": "3f3d6ec2af126324a3004b28846175f2",
+    "url": "/myportfolio-reactjs/static/media/Photo.3f3d6ec2.png"
+  },
+  {
+    "revision": "98d13abd63c9ca1beaf6719e7e7daed7",
+    "url": "/myportfolio-reactjs/static/media/photo-profile.98d13abd.png"
+  },
+  {
+    "revision": "31182ae1996ee7666a47ba0531338d0e",
+    "url": "/myportfolio-reactjs/static/media/resume.31182ae1.pdf"
+  }
+]);
